9c187cc8fb0f9ce6aecbd1adab0b0d65eb05cdd6
jason.m.felice <jason.m.felice@ee7dc0d6-9d84-11dd-8e58-870c635edf7a>
2008-10-20 04:02:14 +0000

    Parse identifiers

--- trunk/src/preprocessing_token_iterator.h
+++ trunk/src/preprocessing_token_iterator.h
@@ -35,6 +35,27 @@ private:
 
     typedef pair<token, base_type> result_type;
 
+    inline static bool is_basic_source_character(wchar_t c)
+    {
+        // This is pre-sorted.  FIXME: 
+        static const wchar_t BASIC[] = 
+            L"\t\n\v\f !\"#%&'()*+,-./0123456789:;<=>?"
+            L"ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
+            L"abcdefghijklmnopqrstuvwxyz{|}~";
+
+        return binary_search(BASIC, BASIC+96, c);
+    }
+
+    inline static bool is_identifier_first_char(wchar_t c)
+    {
+        return iswalpha(c) || c == L'_' || !is_basic_source_character(c);
+    }
+
+    inline static bool is_identifier_char(wchar_t c)
+    {
+        return is_identifier_first_char(c) || iswdigit(c);
+    }
+
     result_type parse()
     {
         base_type i(this->base_reference());
@@ -46,10 +67,23 @@ private:
                 spelling += *i;
                 ++i;
             }
-            return result_type(token(token::WHITESPACE, spelling, p), i);
+            return result_type(token(token::PP_WHITESPACE, spelling, p), i);
+        }
+        if (is_identifier_first_char(*i)) {
+            wstring spelling;
+            while (i != this->end_reference() && is_identifier_char(*i)) {
+                spelling += *i;
+                ++i;
+            }
+            return result_type(token(token::PP_IDENTIFIER, spelling, p), i);
         }
 
-        //FIXME:
+        {
+            wstring spelling;
+            spelling += *i;
+            ++i;
+            return result_type(token(token::PP_OTHER_CHAR, spelling, p), i);
+        }
     }
 };
 
