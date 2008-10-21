c43e4192b90fda7a39af8be93ebd382ca4b3341f
jason.m.felice <jason.m.felice@ee7dc0d6-9d84-11dd-8e58-870c635edf7a>
2008-10-21 03:54:37 +0000

    Correctly identify universal characters which could be part of an identifier.

---
+++ trunk/src/parsing_algorithms.h
@@ -0,0 +1,11 @@
+#ifndef __parsing_algorithms_h__
+#define __parsing_algorithms_h__
+
+namespace vigilance {
+
+bool is_identifier_character(wchar_t c);
+bool is_identifier_first_character(wchar_t c);
+
+}
+
+#endif // ndef __parsing_algorithms_h__
